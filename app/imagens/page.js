2b585e320f0753d4d1869eced72f6d1d8ef86619
Claiton Wrobleswki <claitonwr17-sys@gmail.com>
2026-05-19 16:08:00 -0300

    Adicionando avatar no feed

--- app/imagens/page.js
+++ app/imagens/page.js
@@ -64,7 +64,7 @@ export default function Imagens() {
           <div key={index} style={styles.card}>
             {/* USUÁRIO */}
             <div style={styles.userInfo}>
-              <img src="/logo.png" alt="Logo" style={styles.avatarImage} />
+             <img src="/insta.png" alt="Perfil" style={styles.avatarImage} />
 
               <div>
                 <div style={styles.username}>Claiton Wroblewski</div>
