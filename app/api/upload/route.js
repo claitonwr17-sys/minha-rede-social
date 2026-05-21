7b99818cb4a71823bc11e9cdc7a87e9073a41032
Claiton Wrobleswki <claitonwr17-sys@gmail.com>
2026-05-21 09:49:25 -0300

    Corrigindo upload Cloudinary

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -2,8 +2,8 @@ import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: 794844927554976
+  api_secret: KLrjABwMDCZC4x3xwzyOTZm7oZQ
 });
 
 export async function POST(req) {
