8c65acf43194b4e2592c30dd2c9e50fd3f4953ed
Claiton Wrobleswki <claitonwr17-sys@gmail.com>
2026-05-21 11:12:26 -0300

    corrigindo nome da api key

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -2,10 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: process.env.CHAVE_API_NA_NUVINARY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 export async function POST(req) {
   try {
     const data = await req.formData();
