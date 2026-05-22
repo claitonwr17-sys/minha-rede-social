d3a39e9ba661132ff33a2b63fb90aa6ca4220baa
Claiton Wrobleswki <claitonwr17-sys@gmail.com>
2026-05-22 12:53:12 -0300

    fix upload cloudinary

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,5 +1,63 @@
-export async function GET() {
-  return Response.json({
-    funcionando: true,
-  });
+import { v2 as cloudinary } from "cloudinary";
+
+export const runtime = "nodejs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export async function POST(req) {
+  try {
+    const data = await req.formData();
+    const file = data.get("file");
+
+    if (!file) {
+      return Response.json(
+        {
+          error: "Nenhum arquivo enviado",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const resultado = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            folder: "conrad",
+          },
+          (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result);
+            }
+          }
+        )
+        .end(buffer);
+    });
+
+    return Response.json({
+      url: resultado.secure_url,
+    });
+
+  } catch (erro) {
+    console.log(erro);
+
+    return Response.json(
+      {
+        error: "Erro no upload",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
